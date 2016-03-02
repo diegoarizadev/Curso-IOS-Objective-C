0e427643e510ab831beff03b67b81677cdca39da
Diego Ariza <diego.ariza2@gmail.com>
2016-03-01 21:11:43 -0500

    Componente UIImageView
    Proyecto con la implementación de un componente UIIMageView

---
+++ 4 Class UIImageView/UIImageView/UIImageView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIImageView
+//
+//  Created by n0rf3n on 1/03/16.
+//  Copyright © 2016 n0rf3n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
