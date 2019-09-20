5d40da771d21836548b0aa6966ec00addfe82bd5
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-19 19:57:13 -0500

    🚥 Se agregan clases de CocoaPods con UITableViewController para customizar la tabla
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   9 UITableView/9 UITableView/CustomPlanetTabletViewController.h
    	new file:   9 UITableView/9 UITableView/CustomPlanetTabletViewController.m

---
+++ 9 UITableView/9 UITableView/CustomPlanetTabletViewController.h
@@ -0,0 +1,17 @@
+//
+//  CustomPlanetTabletViewController.h
+//  9 UITableView
+//
+//  Created by Alejandro Ariza on 9/19/19.
+//  Copyright © 2019 Alejandro Ariza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomPlanetTabletViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
