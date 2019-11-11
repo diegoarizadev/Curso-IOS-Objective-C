b989e8bf1ff03591f372e84550d294ad84a8d491
Alejandro Ariza <diego.ariza2@gmail.com>
2019-11-11 16:50:25 -0500

    ✈️ Se agrega animación.
     On branch Develop
     Your branch is up to date with 'origin/Develop'.
    
     Changes to be committed:
    	modified:   21 HidenView/21 HidenView.xcodeproj/project.xcworkspace/xcuserdata/diego.ariza.xcuserdatad/UserInterfaceState.xcuserstate
    	modified:   21 HidenView/21 HidenView/ViewController.swift

--- 21 HidenView/21 HidenView/ViewController.swift
+++ 21 HidenView/21 HidenView/ViewController.swift
@@ -22,20 +22,39 @@ class ViewController: UIViewController {
     @IBAction func Btnhidden(_ sender: Any) {
         if (ViewOrange.isHidden) {
             NSLog("N0rf3nm1");
-            ViewOrange.isHidden = false;
-            test1.constant = 223;
+            //ViewOrange.isHidden = false;
+            //test1.constant = 223;
+       
+            
+            self .setView(view: ViewOrange, hidden: false, state:223, alfaEfect: 1);
 
         }else{
              NSLog("N0rf3nm2");
-            ViewOrange.isHidden = true;
-            test1.constant = 0;
+            //ViewOrange.isHidden = true;
+            //test1.constant = 0;
             
+            self .setView(view: ViewOrange, hidden: true, state:0, alfaEfect:0.0);
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func setView(view: UIView, hidden: Bool, state: CGFloat, alfaEfect: CGFloat) {
+       /*( UIView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            view.isHidden = hidden
+            self.test1.constant = state
+            view.alpha = alfaEfect
+        })
+        */
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .transitionCrossDissolve , animations: {
+            view.isHidden = hidden
+            self.test1.constant = state
+            view.alpha = alfaEfect
+        })
     }
 
  
